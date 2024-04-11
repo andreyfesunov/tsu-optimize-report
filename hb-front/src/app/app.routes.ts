4f03c0c721700c78edbce491e2eb3c772213f8fb
Aleksandr787 <shuverov.aleksandr@gmail.com>
2024-04-12 00:54:07 +0300

    master: add reports component (tabiki)

--- hb-front/src/app/app.routes.ts
+++ hb-front/src/app/app.routes.ts
@@ -1,10 +1,21 @@
 import {Routes} from '@angular/router';
+import { ReportsComponent } from './components/reports/reports.component';
 
 export enum AppRoutes {
   USER_AREA = 'user-area'
 }
 
 export const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'reports'
+  },
+  {
+    path: 'reports',
+    component: ReportsComponent,
+    // canActivate: [authGuard],  
+},
   {
     path: AppRoutes.USER_AREA,
     loadChildren: () => import('./processes/work-area/work-area.routes').then(m => m.routes)
