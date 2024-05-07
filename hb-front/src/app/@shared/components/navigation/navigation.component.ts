cc4b707cb91f386f5e4bc70bcda0b18cfb7bb17c
Aleksandr787 <shuverov.aleksandr@gmail.com>
2024-05-08 01:36:06 +0300

    master: verstka navigation

--- hb-front/src/app/@shared/components/navigation/navigation.component.ts
+++ hb-front/src/app/@shared/components/navigation/navigation.component.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
 import { Router, RouterModule } from '@angular/router';
@@ -11,39 +12,38 @@ import { AuthState, IUser } from '@shared/index';
   imports: [
     CommonModule,
     MatListModule,
+    MatButtonModule,
     MatIconModule,
     RouterModule,
   ],
   template: `
     <div class="navigation">
-      <div class="navigation__category">
-        <mat-nav-list>
-          <a mat-list-item routerLink="main" routerLink="main" routerLinkActive="mdc-list-item--activated">
+        <nav class="navigation__category">
+          <a mat-button class="item-wrapper" routerLink="main" routerLink="main" routerLinkActive="mdc-list-item--activated">
             <div class="navigation__category__item">
               <mat-icon class="navigation__category__item__icon material-symbols-outlined">person</mat-icon>
               <span>Профиль</span>
             </div>
           </a>
-          <a mat-list-item routerLink="products" routerLinkActive="mdc-list-item--activated">
+          <a mat-button class="item-wrapper" routerLink="products" routerLinkActive="mdc-list-item--activated">
             <div class="navigation__category__item">
               <mat-icon class="navigation__category__item__icon material-symbols-outlined">grocery</mat-icon>
               <span>Продукты</span>
             </div>
           </a>
-          <a mat-list-item routerLink="calendar" routerLinkActive="mdc-list-item--activated">
+          <a mat-button class="item-wrapper" routerLink="calendar" routerLinkActive="mdc-list-item--activated">
             <div class="navigation__category__item">
               <mat-icon class="navigation__category__item__icon material-symbols-outlined">calendar_month</mat-icon>
               <span>Календарь</span>
             </div>
           </a>
-          <a mat-list-item (click)="logout();">
+          <a mat-button class="item-wrapper" (click)="logout();">
             <div class="navigation__category__item">
               <mat-icon class="navigation__category__item__icon material-symbols-outlined">logout</mat-icon>
               <span>Выйти</span>
             </div>
           </a>
-        </mat-nav-list>
-      </div>
+        </nav>
     </div>
   `,
   styleUrl: './navigation.component.scss'
