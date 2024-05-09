a50d9b1c17695cad4bb663f15ea0a2a928d47fe5
andrey_fesunov <fsnv04@gmail.com>
2024-05-10 00:46:20 +0300

    fix bat

--- hb-docker/scripts/bat/docker-compose-links.bat
+++ hb-docker/scripts/bat/docker-compose-links.bat
@@ -1,14 +1,15 @@
 @echo off
-REM Set the code page to handle Cyrillic characters
-chcp 1251 > nul
 
-REM Get the absolute path of the script's directory
-for %%I in ("%~dp0.") do set "parent_path=%%~fI"
+chcp 65001
 
-set "backend_folder=%parent_path%\..\..\..\hb-back"
+set "parent_path=%~dp0"
+
+set "backend_folder=%parent_path%\..\..\hb-back"
 set "solution_file=BackendBase.sln"
 echo %backend_folder%
 
-cd /d %backend_folder%
+cd /d "%backend_folder%"
 
-mklink /h "%~dp0\hb-back\hb-docker\docker-compose.yml" "..\hb-docker\docker-compose.yml"
+REM Create symbolic links using mklink command
+mklink docker-compose.yml "..\hb-docker\docker-compose.yml"
+mklink docker-compose.override.yml "..\hb-docker\docker-compose.override.yml"
