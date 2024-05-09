0a5eb4381a975d6f189e6ccd6f3dea89066f96cb
andrey_fesunov <fsnv04@gmail.com>
2024-05-10 00:48:40 +0300

    fix bat

--- hb-docker/scripts/bat/docker-compose-links.bat
+++ hb-docker/scripts/bat/docker-compose-links.bat
@@ -4,7 +4,7 @@ chcp 65001
 
 set "parent_path=%~dp0"
 
-set "backend_folder=%parent_path%\..\..\hb-back"
+set "backend_folder=%parent_path%..\..\..\hb-back"
 set "solution_file=BackendBase.sln"
 echo %backend_folder%
 
