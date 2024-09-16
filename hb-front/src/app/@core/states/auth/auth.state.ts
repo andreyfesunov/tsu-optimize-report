162dc280a7afabc380b09ab23a932714dc66463f
Aleksandr787 <shuverov.aleksandr@gmail.com>
2024-09-16 10:40:30 +0300

    master: fix auth state

--- hb-front/src/app/@core/states/auth/auth.state.ts
+++ hb-front/src/app/@core/states/auth/auth.state.ts
@@ -5,6 +5,23 @@ import {ITokenModel} from "@core/models";
 
 @Injectable({providedIn: "root"})
 export class AuthState {
+  private readonly _tokenKey: string = "JWT_TOKEN";
+
+  public readonly tokenRaw$: Observable<string | undefined> = fromLocalStorage(this._tokenKey).pipe(
+    shareReplay({
+      bufferSize: 1,
+      refCount: false
+    })
+  );
+
+  public readonly token$: Observable<string> = this.tokenRaw$.pipe(
+    exists(),
+    shareReplay({
+      bufferSize: 1,
+      refCount: true
+    })
+  );
+
   public readonly user$: Observable<ITokenModel> = this.token$.pipe(
     map((token) => parseJwt(token)),
     shareReplay({
@@ -12,22 +29,9 @@ export class AuthState {
       refCount: true
     })
   );
-  private readonly _tokenKey: string = "JWT_TOKEN";
-  public readonly tokenRaw$: Observable<string | undefined> = fromLocalStorage(this._tokenKey).pipe(
-    shareReplay({
-      bufferSize: 1,
-      refCount: false
-    })
-  );
+
   public readonly valid$: Observable<boolean> = this.tokenRaw$.pipe(
     map((token) => isTokenValid(token))
-  );
-  public readonly token$: Observable<string> = this.tokenRaw$.pipe(
-    exists(),
-    shareReplay({
-      bufferSize: 1,
-      refCount: true
-    })
   );
 
   public setToken(accessToken: string): void {
