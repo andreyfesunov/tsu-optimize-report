8fe2e9883eb1ddc2e301215934cbd250dabcd3eb
ivan <iambagger10@gmail.com>
2025-03-29 23:58:26 +0300

    Добавление всех ставок в бд ставок

--- hb-back/Tsu.IndividualPlan.Data/Data/Migrations/20250329205331_AddJobs.Designer.cs
+++ hb-back/Tsu.IndividualPlan.Data/Data/Migrations/20250329205331_AddJobs.Designer.cs
@@ -2,18 +2,21 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using Tsu.IndividualPlan.Data.Context;
 
 #nullable disable
 
-namespace Tsu.IndividualPlan.Data.Migrations
+namespace Tsu.IndividualPlan.Data.Data.Migrations
 {
     [DbContext(typeof(DataContext))]
-    partial class DataContextModelSnapshot : ModelSnapshot
+    [Migration("20250329205331_AddJobs")]
+    partial class AddJobs
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
