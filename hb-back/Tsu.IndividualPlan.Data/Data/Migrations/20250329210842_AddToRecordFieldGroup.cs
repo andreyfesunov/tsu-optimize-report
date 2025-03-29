d1928739b3ba64d55198cc657c1b9c1d19b4fee2
ivan <iambagger10@gmail.com>
2025-03-30 00:14:03 +0300

    Добавление проперти Group в Record для определения к какой группе принадлежит активность из строчки отчёта

---
+++ hb-back/Tsu.IndividualPlan.Data/Data/Migrations/20250329210842_AddToRecordFieldGroup.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Tsu.IndividualPlan.Data.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddToRecordFieldGroup : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Group",
+                table: "Records",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "GroupString",
+                table: "Records");
+        }
+    }
+}
