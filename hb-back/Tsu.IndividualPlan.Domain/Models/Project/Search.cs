b690f91812c4f72ac8c935c0e64716bdde56525d
ivan <iambagger10@gmail.com>
2025-07-16 20:11:41 +0300

    Fix empty states view

--- hb-back/Tsu.IndividualPlan.Domain/Models/Project/Search.cs
+++ hb-back/Tsu.IndividualPlan.Domain/Models/Project/Search.cs
@@ -1,7 +1,8 @@
 ﻿namespace Tsu.IndividualPlan.Domain.Models.Project;
 
-public class Search(int PageNumber, int PageSize)
+public class Search(int PageNumber, int PageSize, bool IsActive = true)
 {
     public int PageNumber { get; init; } = PageNumber;
     public int PageSize { get; init; } = PageSize;
+    public bool IsActive { get; init; } = IsActive;
 }
